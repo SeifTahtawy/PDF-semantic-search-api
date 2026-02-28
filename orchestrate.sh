02e2d75d5a1e8ffa058bea124426bbb039ed78fb
SeifTahtawy <seiftahtawy@gmail.com>
2026-02-28 04:47:09 +0200

    Add orchestration script.

---
+++ orchestrate.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ "$1" != "--action" || -z "$2" ]]; then
+    echo "Error: Invalid command usage."
+    echo
+    echo "Correct usage:"
+    echo "  ./orchestrate.sh --action start"
+    echo "  ./orchestrate.sh --action terminate"
+    exit 1
+fi
+
+if [[ "$2" == "start" ]]; then
+    docker compose up --build -d
+elif [[ "$2" == "terminate" ]]; then
+    docker compose down -v
+else
+    echo "Error: Invalid action '$2'."
+    echo
+    echo "Valid actions are:"
+    echo "  start"
+    echo "  terminate"
+    exit 1
+fi
